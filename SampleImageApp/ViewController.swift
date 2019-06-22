b637d381ce501a0b4343be22cc44be5831660a53
Neha Pandey <npandey22@dxc.com>
2019-06-22 10:35:09 +0530

    Initial Commit

---
+++ SampleImageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleImageApp
+//
+//  Created by Neha Pandey on 22/06/19.
+//  Copyright © 2019 Neha Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
